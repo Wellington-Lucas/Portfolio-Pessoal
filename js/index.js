bf96cd9a4a515d3e94dc7a19b21c0805295a07c9
Wellington-Lucas <wellingtongit.1@gmail.com>
2023-02-06 19:33:10 -0300

    Atualizei as mediaQueries
    Atualizei as mediaQueries e atualizei o botão "menu hamburger".

--- js/index.js
+++ js/index.js
@@ -1,3 +1,32 @@
+var mobileButton = document.getElementById('menu-button')
+var mobileNav = document.getElementById('nav-menu-mobile')
+
+
+function openMenu() {
+    
+    mobileNav.classList.toggle('opened')
+    
+}
+
+mobileButton.addEventListener('click', openMenu)
+
+
+function closeMenu() {
+    mobileNav.classList.remove('opened')
+
+}
+
+
+
+var menuMobileLink = document.getElementsByClassName('mobileLink')
+
+Array.from(menuMobileLink).forEach((menuMobileLink) => {
+    
+    menuMobileLink.addEventListener('click', closeMenu)
+
+});
+
+
 function changeTheme() {
 
     var bodyDoc = document.body 
